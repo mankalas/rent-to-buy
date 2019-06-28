20e9e42402b84e01573d4ac7e0d627d0f46f6173
Vincent Boucheny <vincent@Vincents-MacBook-Perso.local>
2019-06-28 22:51:00 +1200

    Loan Tests v2.0!

--- src/Loan.elm
+++ src/Loan.elm
@@ -1,84 +1,136 @@
-module Loan exposing (Frequency(..), Model, interests, interestsAt, total, wInM, wInY, wInterest, wPayments, wRate)
-
-
-type Frequency
-    = Weekly
-    | Monthly
-    | Yearly
+module Loan exposing (Model, amortBegBalance, amortEndBalance, amortInterest, amortPrincipal, cost, interest, payment, paymentsNumber)
 
 
 type alias Model =
-    { term : Int -- Months
-    , interest_rate : Float -- [0..1]
+    { term : Int -- Weeks
+    , interest_rate : Float -- [0..1] p.a.
     , amount : Float -- $
-    , coumpound : Frequency
-    , pay_back : Frequency
     }
 
 
+wInY : Float
 wInY =
     52
 
 
-wInM =
-    4.348214
+w_interest_rate : Model -> Float
+w_interest_rate loan =
+    loan.interest_rate / wInY
 
 
-wPayments : Model -> Float
-wPayments loan =
+w_interest : Model -> Float
+w_interest loan =
+    w_interest_rate loan * loan.amount
+
+
+paymentsNumber : Model -> Int
+paymentsNumber loan =
+    loan.term
+
+
+payment : Model -> Float
+payment loan =
     let
         n =
-            toFloat loan.term * wInY
+            paymentsNumber loan |> toFloat
 
-        i =
-            loan.interest_rate / toFloat 100 / wInY
+        r =
+            w_interest_rate loan
 
         d =
-            (((1 + i) ^ n) - 1) / (i * (1 + i) ^ n)
+            r / (1 - ((1 + r) ^ -n))
     in
-    loan.amount / d
+    loan.amount * d
 
 
-wRate lr =
-    lr / toFloat 100 / wInY
+w_principal : Model -> Float -> Float
+w_principal loan pay =
+    pay - w_interest loan
 
 
-wInterest lr la =
-    wRate lr * la
-
-
-interestsAt : Model -> Float -> Int -> Float
-interestsAt loan pay n =
+interest_ : Model -> Float -> Float
+interest_ loan pay =
     let
-        weekly_rate =
-            wRate loan.interest_rate
+        wi =
+            w_interest loan
 
-        weekly_interest =
-            weekly_rate * loan.amount
+        wp =
+            w_principal loan pay
 
-        principal_pay =
-            pay - weekly_interest
+        new_loan =
+            { loan
+                | term = loan.term - 1
+                , amount = loan.amount - wp
+            }
     in
-    if loan.term == n || loan.term <= 0 then
+    if loan.term == 0 then
         0
 
     else
-        let
-            new_loan =
-                { loan | term = loan.term - 1, amount = loan.amount - principal_pay }
-        in
-        weekly_interest + interestsAt new_loan pay n
+        wi + interest_ new_loan pay
 
 
-interests : Model -> Float
-interests loan =
+interest : Model -> Float
+interest loan =
+    interest_ loan (payment loan)
+
+
+afterPay : Model -> Float -> Model
+afterPay loan pay =
     let
-        w_pay =
-            wPayments loan
+        wp =
+            w_principal loan pay
     in
-    interestsAt loan w_pay 0
+    { loan
+        | term = loan.term - 1
+        , amount = loan.amount - wp
+    }
 
 
-total : Model -> Float
-total loan =
-    loan.amount + interests loan
+cost : Model -> Float
+cost loan =
+    interest loan + loan.amount
+
+
+atNthIncrement : Model -> Int -> Float -> Model
+atNthIncrement loan n pay =
+    if n == 1 then
+        loan
+
+    else
+        atNthIncrement (afterPay loan pay) (n - 1) pay
+
+
+amortBegBalance : Model -> Int -> Float
+amortBegBalance loan n =
+    let
+        new_loan =
+            atNthIncrement loan n (payment loan)
+    in
+    new_loan.amount
+
+
+amortEndBalance : Model -> Int -> Float
+amortEndBalance loan n =
+    amortBegBalance loan (n + 1)
+
+
+amortInterest : Model -> Int -> Float
+amortInterest loan n =
+    let
+        new_loan =
+            atNthIncrement loan n (payment loan)
+    in
+    new_loan.interest_rate / wInY * new_loan.amount
+
+
+amortPrincipal : Model -> Int -> Float
+amortPrincipal loan n =
+    let
+        pay =
+            payment loan
+
+        new_loan =
+            atNthIncrement loan n pay
+    in
+    w_principal new_loan pay
--- tests/LoanTests.elm
+++ tests/LoanTests.elm
@@ -10,165 +10,77 @@ suite : Test
 suite =
     let
         loan =
-            Loan.Model (52 * 20 + 4) 0.05 500000 Loan.Yearly
+            Loan.Model (52 * 20) 0.05 500000
     in
     describe "The Loan module"
-        [ describe "Payment"
-            [ test "Correct weekly value" <|
-                \_ ->
-                    Loan.payment loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 745.97
-            , test "Correct monthly value" <|
-                \_ ->
-                    Loan.payment loan Loan.Monthly
-                        |> Expect.within (Expect.Absolute 1) 3237.59
-            , test "Correct yearly value" <|
-                \_ ->
-                    Loan.payment loan Loan.Yearly
-                        |> Expect.within (Expect.Absolute 1) 39733.6
-            ]
-        , describe "Total number of payments"
+        [ describe "Total number of payments"
             [ test "Weekly payments" <|
                 \_ ->
-                    Loan.paymentsNumber loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 1057.33
-            , test "Monthly payments" <|
+                    Loan.paymentsNumber loan
+                        |> Expect.equal 1040
+            ]
+        , describe "Payment"
+            [ test "Correct weekly value" <|
                 \_ ->
-                    Loan.paymentsNumber loan Loan.Monthly
-                        |> Expect.within (Expect.Absolute 1) 244
-            , test "Yearly payments" <|
-                \_ ->
-                    Loan.paymentsNumber loan Loan.Yearly
-                        |> Expect.within (Expect.Absolute 1) 20.33
+                    Loan.payment loan
+                        |> Expect.within (Expect.Absolute 1) 760.78
             ]
         , describe "Total interest"
             [ test "Weekly payments" <|
                 \_ ->
-                    Loan.interest loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 288737.53
-            , test "Monthly payments" <|
-                \_ ->
-                    Loan.interest loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 289972.43
-            , test "Yearly payments" <|
-                \_ ->
-                    Loan.interest loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 307916.49
+                    Loan.interest loan
+                        |> Expect.within (Expect.Absolute 1.5) 291208.6
             ]
         , describe "Total cost"
             [ test "Weekly payments" <|
                 \_ ->
-                    Loan.cost loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 788737.53
-            , test "Monthly payments" <|
-                \_ ->
-                    Loan.cost loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 789972.43
-            , test "Yearly payments" <|
-                \_ ->
-                    Loan.cost loan Loan.Weekly
-                        |> Expect.within (Expect.Absolute 1) 807916.49
+                    Loan.cost loan
+                        |> Expect.within (Expect.Absolute 1.5) 791208.6
             ]
         , describe "Amortisation table"
             [ describe "Beginning balance"
                 [ describe "First increment"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortBegBalance loan Loan.Weekly 1
-                                |> Expect.within (Expect.Absolute 1) 500000
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Monthly 1
-                                |> Expect.within (Expect.Absolute 1) 500000
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Yearly 1
-                                |> Expect.within (Expect.Absolute 1) 500000
+                            Loan.amortBegBalance loan 1
+                                |> Expect.within (Expect.Absolute 0.01) 500000
                     ]
                 , describe "Halfway"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortBegBalance loan Loan.Weekly 529
-                                |> Expect.within (Expect.Absolute 1) 311065.95
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Monthly 122
-                                |> Expect.within (Expect.Absolute 1) 312726.85
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Yearly 10
-                                |> Expect.within (Expect.Absolute 1) 337539.04
+                            Loan.amortBegBalance loan 529
+                                |> Expect.within (Expect.Absolute 1) 307495.7
                     ]
                 , describe "Last increment"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortBegBalance loan Loan.Weekly 1058
-                                |> Expect.within (Expect.Absolute 1) 248.5
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Monthly 244
-                                |> Expect.within (Expect.Absolute 1) 3224.46
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortBegBalance loan Loan.Yearly 21
-                                |> Expect.within (Expect.Absolute 1) 12819.53
+                            Loan.amortBegBalance loan 1040
+                                |> Expect.within (Expect.Absolute 1) 760
                     ]
                 ]
             , describe "Ending balance"
                 [ describe "First increment"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortEndBalance loan Loan.Weekly 1
-                                |> Expect.within (Expect.Absolute 1) 499723.39
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Monthly 1
-                                |> Expect.within (Expect.Absolute 1) 500000
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Yearly 1
-                                |> Expect.within (Expect.Absolute 1) 500000
+                            Loan.amortEndBalance loan 1
+                                |> Expect.within (Expect.Absolute 0.01) 499719.99
                     ]
                 , describe "Halfway"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortEndBalance loan Loan.Weekly 529
-                                |> Expect.within (Expect.Absolute 1) 310611.98
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Monthly 122
-                                |> Expect.within (Expect.Absolute 1) 312726.85
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Yearly 10
-                                |> Expect.within (Expect.Absolute 1) 337539.04
+                            Loan.amortEndBalance loan 529
+                                |> Expect.within (Expect.Absolute 1) 307030.59
                     ]
                 , describe "Before last increment"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortEndBalance loan Loan.Weekly 1057
-                                |> Expect.within (Expect.Absolute 1) 248.5
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Monthly 244
-                                |> Expect.within (Expect.Absolute 1) 3224.46
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Yearly 21
-                                |> Expect.within (Expect.Absolute 1) 12819.53
+                            Loan.amortEndBalance loan 1039
+                                |> Expect.within (Expect.Absolute 1) 760
                     ]
                 , describe "Last increment"
                     [ test "Weekly payments" <|
                         \_ ->
-                            Loan.amortEndBalance loan Loan.Weekly 1058
-                                |> Expect.within (Expect.Absolute 1) 0
-                    , test "Monthly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Monthly 244
-                                |> Expect.within (Expect.Absolute 1) 0
-                    , test "Yearly payments" <|
-                        \_ ->
-                            Loan.amortEndBalance loan Loan.Yearly 21
+                            Loan.amortEndBalance loan 1040
                                 |> Expect.within (Expect.Absolute 1) 0
                     ]
                 ]
@@ -177,88 +89,40 @@ suite =
             [ describe "First increment"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortInterest loan Loan.Weekly 1
-                            |> Expect.within (Expect.Absolute 1) 469.36
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Monthly 1
-                            |> Expect.within (Expect.Absolute 1) 500000
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Yearly 1
-                            |> Expect.within (Expect.Absolute 1) 500000
+                        Loan.amortInterest loan 1
+                            |> Expect.within (Expect.Absolute 1) 480.77
                 ]
             , describe "Halfway"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortInterest loan Loan.Weekly 529
-                            |> Expect.within (Expect.Absolute 1) 292
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Monthly 122
-                            |> Expect.within (Expect.Absolute 1) 312726.85
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Yearly 10
-                            |> Expect.within (Expect.Absolute 1) 337539.04
+                        Loan.amortInterest loan 529
+                            |> Expect.within (Expect.Absolute 1) 295.67
                 ]
             , describe "Last increment"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortInterest loan Loan.Weekly 1058
-                            |> Expect.within (Expect.Absolute 1) 0.23
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Monthly 244
-                            |> Expect.within (Expect.Absolute 1) 3224.46
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortInterest loan Loan.Yearly 21
-                            |> Expect.within (Expect.Absolute 1) 12819.53
+                        Loan.amortInterest loan 1040
+                            |> Expect.within (Expect.Absolute 1) 0.73
                 ]
             ]
         , describe "Principal"
             [ describe "First increment"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortPrincipal loan Loan.Weekly 1
-                            |> Expect.within (Expect.Absolute 1) 3
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Monthly 1
-                            |> Expect.within (Expect.Absolute 1) 500000
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Yearly 1
-                            |> Expect.within (Expect.Absolute 1) 500000
+                        Loan.amortPrincipal loan 1
+                            |> Expect.within (Expect.Absolute 1) 280.01
                 ]
             , describe "Halfway"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortPrincipal loan Loan.Weekly 529
-                            |> Expect.within (Expect.Absolute 1) 453.97
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Monthly 122
-                            |> Expect.within (Expect.Absolute 1) 312726.85
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Yearly 10
-                            |> Expect.within (Expect.Absolute 1) 337539.04
+                        Loan.amortPrincipal loan 529
+                            |> Expect.within (Expect.Absolute 1) 465.11
                 ]
             , describe "Last increment"
                 [ test "Weekly payments" <|
                     \_ ->
-                        Loan.amortPrincipal loan Loan.Weekly 1058
-                            |> Expect.within (Expect.Absolute 1) 248.5
-                , test "Monthly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Monthly 244
-                            |> Expect.within (Expect.Absolute 1) 3224.46
-                , test "Yearly payments" <|
-                    \_ ->
-                        Loan.amortPrincipal loan Loan.Yearly 21
-                            |> Expect.within (Expect.Absolute 1) 12819.53
+                        Loan.amortPrincipal loan 1040
+                            |> Expect.within (Expect.Absolute 1) 760
                 ]
             ]
         ]
